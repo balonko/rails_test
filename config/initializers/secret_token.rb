917dc4ab04fbbe606e6e119fc9d672aab8639f34
Toni Cesarek <toni.cesarek@gmail.com>
2013-10-19 08:20:25 +0200

    začetni komit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tonko::Application.config.secret_key_base = '10e5aac5c1d9759cb12e4941af8396a4446084851643e0daf349bb597ba8303c308286b402c0dbe897a97846b514e3fc397c00eb7186d6d749e677791aa7157d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Tonko::Application.config.session_store :cookie_store, key: '_tonko_session'
